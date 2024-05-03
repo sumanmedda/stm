3b21295e8033c78dc9c1061f35274214ba9ab7f1
sumanmedda <sumanmedda33@gmail.com>
2024-05-03 09:59:04 +0530

    updated graph

--- main.py
+++ main.py
@@ -2,4 +2,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas_datareader as data
+import yfinance as yf
+
+# setting the start and end date
+start_date = "2023-01-01"
+end_date = pd.Timestamp.today().strftime('%Y-%m-%d')  # Get today's date
+# set stock data
+ticker = "BTC-USD"
+df = yf.download(ticker, start=start_date, end=end_date, auto_adjust=True)
+
+print(df.head())
